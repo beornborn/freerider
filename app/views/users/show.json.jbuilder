4b4ad7de4a73e98e5000cea21bdac518b7f09884
Oleg Gorbunov <beornborn@gmail.com>
2016-04-24 09:37:06 +0300

    add user model

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :user_session, :wins_count, :games_count, :game_id, :point, :online, :created_at, :updated_at
---
+++ db/migrate/20160423194617_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :user_session
+      t.integer :wins_count, default: 0
+      t.integer :games_count, default: 0
+      t.integer :game_id
+      t.integer :point, default: 0
+      t.boolean :online, default: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :game_id
+  end
+end

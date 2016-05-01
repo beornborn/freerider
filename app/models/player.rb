dcc48fd30b5be2d5d94c3ce426c90f00a4b9b199
Oleg Gorbunov <beornborn@gmail.com>
2016-05-01 13:46:06 +0300

    show update in game list in real-time

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,6 +9,17 @@ class Player < ApplicationRecord
   scope :cool, -> { where(hitrojop: false) }
   scope :winners, -> { where(winner: true) }
 
+  after_create -> { ActionCable.server.broadcast 'game_list', {
+      msg: 'refresh',
+      games: Game.includes(players: :user).order(id: :desc).map {|g| game_as_json(g) }
+    } }
+
+  def game_as_json(game)
+    game.as_json(only: [:id, :name, :rounds, :time_to_think, :players_amount]).tap do |g|
+      g[:players] = game.players.as_json(only: [:id, :winner], methods: [:name])
+    end
+  end
+
   def decide!(data)
     self.hitrojop = data['hitrojop']
     self.increment(:points) if data['hitrojop']

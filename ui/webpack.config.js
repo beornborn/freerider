96d69b3afff15630ba71baa3fbe86dd9827c6f2b
Oleg Gorbunov <beornborn@gmail.com>
2016-05-15 08:57:20 +0300

    fix web pack assembling issue

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js"
   },
   module: {
@@ -16,7 +16,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loaders: ["style", "css"]
+        loaders: ["style", "css", "sass"]
       }
     ]
   }
